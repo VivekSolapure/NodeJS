c014b7175f6524e8bd77b2da4e72b1a70d6671de
Vivek Solapure <solapurevivek2003@gmail.com>
2024-07-13 01:35:51 +0530

    MERN app: connecting Mongo cloud for deployment

---
+++ chapter6/index.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+require('dotenv').config()
+const productrouter=require('./routes/productRouter.js')
+const usertrouter=require('./routes/userRouter.js')
+const express =require('express');
+const path=require('path')
+const server=express();
+const {Schema}=mongoose;
+const cors=require('cors');
+server.use(express.json())
+
+//db connection
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb+srv://solapurevivek2003:CDJsB66bhVYXNsDA@cluster0.8pptecy.mongodb.net/ecommerceCloud?retryWrites=true&w=majority&appName=Cluster0');
+  console.log("database Connected");
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+
+
+//bodyparser
+server.use(cors());
+
+server.use('/products',productrouter.routes);
+server.use('/users',usertrouter.routes);
+server.use(express.static(path.resolve(__dirname,process.env.PUBLIC_DIR)))
+server.use('*',(req,res)=>{
+  res.sendFile(path.resolve(__dirname,'build','index.html'))
+})
+// console.log(process.env.PASSWORD);
+server.listen(process.env.PORT,()=>{
+    console.log('Server Started');
+})
---
+++ chapter6/routes/productRouter.js
@@ -0,0 +1,13 @@
+const productcontroller=require('../../controller/productController.js')
+const express =require('express')
+const productRouter=express.Router();
+
+productRouter
+.post('/',productcontroller.createProduct)
+.get('/',productcontroller.getAllProducts)
+.get('/:id',productcontroller.getProduct)
+.put('/:id', productcontroller.replaceProduct)
+.patch('/:id',productcontroller.updateProduct)
+.delete('/:id', productcontroller.deleteProduct)
+
+exports.routes=productRouter
